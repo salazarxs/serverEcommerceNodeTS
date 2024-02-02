14bc7ee163f791d3b3f4bfe9971d651364bb566b
salazarxs <martinpaolo10@gmail.com>
2024-02-02 11:54:10 -0300

    send jwt to client

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
 import ProductModel from "../models/product";
 import { Product } from "../types/product";
-import { ValidateJWT } from "../helpers/JWT";
+import { GenerateJWT, ValidateJWT } from "../helpers/JWT";
 
 export const CreateProduct = async (req: Request, res: Response) => {
-  const JWT = req.get("JWT");
+  const JWT = req.get("Authorization");
 
   let validateJWT;
   if (JWT) {
@@ -21,6 +21,8 @@ export const CreateProduct = async (req: Request, res: Response) => {
   try {
     const newProduct = await ProductModel.create(product);
     if (newProduct) {
+      const resJWT = await GenerateJWT(product.productName);
+      res.setHeader("Authorization", resJWT);
       res.status(200).json({
         message: "Product created successful",
       });
@@ -33,7 +35,7 @@ export const CreateProduct = async (req: Request, res: Response) => {
 };
 
 export const FindProductsByQuantity = async (req: Request, res: Response) => {
-  const JWT = req.get("JWT");
+  const JWT = req.get("Authorization");
 
   let validateJWT;
   if (JWT) {
@@ -50,6 +52,8 @@ export const FindProductsByQuantity = async (req: Request, res: Response) => {
         limit: parseInt(quantity || "10", 10),
       });
       if (products) {
+        const resJWT: string = await GenerateJWT("Find products by quantity");
+        res.setHeader("Authorization", resJWT);
         return res.status(200).json({ products });
       }
     } catch (err) {
